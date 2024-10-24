73caa2d21ee1195dd22e836e90c946594e80e9af
DaViDev985 <davidev985@gmail.com>
2024-10-24 19:59:36 +0200

    Build vendor.samsung_ext.hardware.camera.flashlight-service and FlashControl This fixes an issue with the flashlight being dim. Huge thanks to whystarlix (a.k.a. onesmiledx), wherever he is. This includes changes to SEPolicy.

--- common.mk
+++ common.mk
@@ -108,6 +108,11 @@ PRODUCT_PACKAGES += \
 # Dolby
 $(call inherit-product, hardware/dolby/dolby.mk)
 
+# Samsung Ext
+PRODUCT_PACKAGES += \
+    vendor.samsung_ext.hardware.camera.flashlight-service \
+    FlashControl
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey \
@@ -378,6 +383,7 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/pixel \
     hardware/qcom-caf/common/libqti-perfd-client \
     hardware/samsung \
+    hardware/samsung-ext/interfaces \
     vendor/qcom/opensource/usb/etc \
 
 # Telephony
